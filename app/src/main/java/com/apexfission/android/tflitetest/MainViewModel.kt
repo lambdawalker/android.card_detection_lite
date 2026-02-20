fd8d2a9ebda9e1bf825a43fcd1ab2634124365fe
David Garcia <bytesWright@isdavid.com>
2026-02-19 23:38:26 -0700

    Update `MainViewModel` to integrate `OcrWrapper` for processing detected image cutouts.
    - Replace the `cutouts` state flow with `ocrResult` to store and expose OCR processing outcomes.
    - Implement OCR logic in `onDetections` that triggers when detections are available and enabled.
    - Add concurrency handling to `onDetections` to toggle detection state while performing OCR on the first available cutout using `Dispatchers.IO`.
    - Include debug logging for OCR results, including text content and bounding boxes.
    - Remove unnecessary whitespace in `gradle/libs.versions.toml`.

--- app/src/main/java/com/apexfission/android/tflitetest/MainViewModel.kt
+++ app/src/main/java/com/apexfission/android/tflitetest/MainViewModel.kt
@@ -1,23 +1,47 @@
 package com.apexfission.android.tflitetest
 
 import android.graphics.Bitmap
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.apexfission.android.carddetectionlite.domain.ocr.OcrResult
+import com.apexfission.android.carddetectionlite.domain.ocr.OcrWrapper
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainViewModel : ViewModel() {
-
-    private val _cutouts = MutableStateFlow<List<Bitmap>>(emptyList())
-    val cutouts = _cutouts.asStateFlow()
-
     private val _isDetectionEnabled = MutableStateFlow(true)
     val isDetectionEnabled = _isDetectionEnabled.asStateFlow()
 
+    private val _ocrResult = MutableStateFlow<List<OcrResult>>(emptyList())
+    val ocrResult = _ocrResult.asStateFlow()
+
+    private val ocrWrapper = OcrWrapper()
+
     fun onDetections(newCutouts: List<Bitmap>) {
-        viewModelScope.launch {
-            _cutouts.value = newCutouts
+        if (newCutouts.isNotEmpty() && _isDetectionEnabled.value) {
+            _isDetectionEnabled.value = false
+            viewModelScope.launch {
+                try {
+                    val cutout = newCutouts.first()
+
+                    val result = withContext(Dispatchers.IO) {
+                        ocrWrapper.run(cutout)
+                    }
+                    _ocrResult.value = result
+
+                    Log.d("OCR", ">>>>>>>>>>>>> ${cutout.width} ${cutout.height}")
+                    result.forEach {
+                        Log.d("OCR", "${it.text.replace("\n", " ")}, ${it.boundingBox}")
+                    }
+
+                } finally {
+                    _isDetectionEnabled.value = true
+                }
+            }
         }
     }
 
