1eafe0b7889fb7e9da38d533aa5fe0f3684d6101
David Garcia <bytesWright@isdavid.com>
2026-02-19 19:04:24 -0700

    Remove unused `appcompat` and `material` dependencies from the `:tfmodel` module.

--- tfmodel/build.gradle.kts
+++ tfmodel/build.gradle.kts
@@ -43,8 +43,6 @@ dependencies {
     implementation(project(":cardDetectionLite"))
 
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
